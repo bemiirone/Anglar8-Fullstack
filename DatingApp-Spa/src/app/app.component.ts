00bd7fd415ca9fe95f28f5f423093844fee5bbc5
Bemi Irone <bemidev@nsmultimedia.co.uk>
2020-07-08 12:02:48 +0100

    Added theming

--- DatingApp-Spa/src/app/app.component.ts
+++ DatingApp-Spa/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
-import { User } from './'
 import { AuthService } from './services/auth.service';
 
 @Component({
