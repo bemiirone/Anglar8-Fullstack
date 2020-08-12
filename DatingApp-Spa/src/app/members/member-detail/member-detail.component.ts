2028973433c133d840788203b93ac2edcacf5da4
Bemi Irone <bemidev@nsmultimedia.co.uk>
2020-08-12 13:14:02 +0100

    member message functionality added

--- DatingApp-Spa/src/app/members/member-detail/member-detail.component.ts
+++ DatingApp-Spa/src/app/members/member-detail/member-detail.component.ts
@@ -1,9 +1,14 @@
 import { AlertifyService } from './../../services/alertify.service';
 import { UserService } from './../../services/user.service';
 import { User } from './../../models/user';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
+import {
+  NgxGalleryOptions,
+  NgxGalleryImage,
+  NgxGalleryAnimation,
+} from 'ngx-gallery';
+import { TabsetComponent } from 'ngx-bootstrap/tabs';
 
 @Component({
   selector: 'app-member-detail',
@@ -11,6 +16,7 @@ import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gal
   styleUrls: ['./member-detail.component.css'],
 })
 export class MemberDetailComponent implements OnInit {
+  @ViewChild('memberTabs', { static: true }) memberTabs: TabsetComponent;
   user: User;
   galleryOptions: NgxGalleryOptions[];
   galleryImages: NgxGalleryImage[];
@@ -25,6 +31,12 @@ export class MemberDetailComponent implements OnInit {
     this.route.data.subscribe((data) => {
       this.user = data['user'];
     });
+
+    this.route.queryParams.subscribe((params) => {
+      const selectedTab = params['tab'];
+      this.memberTabs.tabs[selectedTab > 0 ? selectedTab : 0].active = true;
+    });
+    
     this.galleryOptions = [
       {
         width: '500px',
@@ -32,8 +44,8 @@ export class MemberDetailComponent implements OnInit {
         imagePercent: 100,
         thumbnailsColumns: 4,
         imageAnimation: NgxGalleryAnimation.Slide,
-        preview: false
-      }
+        preview: false,
+      },
     ];
     this.galleryImages = this.getImages();
   }
@@ -46,19 +58,13 @@ export class MemberDetailComponent implements OnInit {
         small: photo.url,
         medium: photo.url,
         big: photo.url,
-        description: photo.description
+        description: photo.description,
       });
     }
     return imageUrls;
   }
 
-  // loadUser() {
-  //   this.userService
-  //     .getUser(+this.route.snapshot.params['id'])
-  //     .subscribe((user: User) => {
-  //       this.user = user;
-  //     }, error => {
-  //       this.alertify.error(error);
-  //     });
-  // }
+  selectTab(tabID: number) {
+    this.memberTabs.tabs[tabID].active = true;
+  }
 }
