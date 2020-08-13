120421ce485052c8f35ef6bf44f8caee2b7aaad4
Bemi Irone <bemidev@nsmultimedia.co.uk>
2020-08-13 11:33:30 +0100

    Added delete message functionality

--- DatingApp-Spa/src/app/messages/messages.component.ts
+++ DatingApp-Spa/src/app/messages/messages.component.ts
@@ -1,20 +1,20 @@
-import { ActivatedRoute } from '@angular/router';
-import { AlertifyService } from './../services/alertify.service';
-import { AuthService } from './../services/auth.service';
-import { UserService } from './../services/user.service';
-import { Pagination, PaginatedResult } from './../models/pagination';
-import { Component, OnInit } from '@angular/core';
-import { Message } from '../models/message';
+import { ActivatedRoute } from "@angular/router";
+import { AlertifyService } from "./../services/alertify.service";
+import { AuthService } from "./../services/auth.service";
+import { UserService } from "./../services/user.service";
+import { Pagination, PaginatedResult } from "./../models/pagination";
+import { Component, OnInit } from "@angular/core";
+import { Message } from "../models/message";
 
 @Component({
-  selector: 'app-messages',
-  templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.css'],
+  selector: "app-messages",
+  templateUrl: "./messages.component.html",
+  styleUrls: ["./messages.component.css"],
 })
 export class MessagesComponent implements OnInit {
   messages: Message[];
   pagination: Pagination;
-  messageContainer = 'Unread';
+  messageContainer = "Unread";
 
   constructor(
     private userService: UserService,
@@ -24,21 +24,42 @@ export class MessagesComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.route.data.subscribe(data => {
-      this.messages = data['messages'].result;
-      this.pagination = data['messages'].pagination;
+    this.route.data.subscribe((data) => {
+      this.messages = data["messages"].result;
+      this.pagination = data["messages"].pagination;
     });
   }
 
   loadMessages() {
-    this.userService.getMessages(this.authService.decodedToken.nameid, this.pagination.currentPage,
-      this.pagination.itemsPerPage, this.messageContainer)
-      .subscribe((res: PaginatedResult<Message[]>) => {
-        this.messages = res.result;
-        this.pagination = res.pagination;
-      }, error => {
-        this.alertify.error(error);
-      });
+    this.userService
+      .getMessages(
+        this.authService.decodedToken.nameid,
+        this.pagination.currentPage,
+        this.pagination.itemsPerPage,
+        this.messageContainer
+      )
+      .subscribe(
+        (res: PaginatedResult<Message[]>) => {
+          this.messages = res.result;
+          this.pagination = res.pagination;
+        },
+        (error) => {
+          this.alertify.error(error);
+        }
+      );
+  }
+
+  deleteMessage(id: number) {
+    this.alertify.confirm("Are you sure", () => {
+      this.userService
+        .deleteMessage(id, this.authService.decodedToken.nameid)
+        .subscribe(() => {
+          this.messages.splice(this.messages.findIndex(m => m.id === id), 1);
+          this.alertify.success('Message Deleted');
+        }, error => {
+          this.alertify.error('Delete Failed');
+        });
+    });
   }
 
   pageChanged(event: any): void {
